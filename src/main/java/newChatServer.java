f894b299f4730809f757d2b7eb312313a9b2e890
Japerz <jackma12138@gmail.com>
2023-04-30 23:25:51 -0400

    Tried to make file transfer function working
    - Tried to make file transfer function working, for now the file transfer from client to server is working. For now two types of file can be saved, txt and png files. Have to define the location first.
    - The message listener is a todo fix.

--- src/main/java/newChatServer.java
+++ src/main/java/newChatServer.java
@@ -20,6 +20,7 @@ public class newChatServer {
     }
 
     public void start() {
+
         System.out.println("Server started");
 
         while (true) {
@@ -27,8 +28,6 @@ public class newChatServer {
                 // Wait for a new client to connect
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("New client connected: " + clientSocket);
-
-
 
                 // Create a new ClientHandler thread to handle the client's connection
                 ClientHandler clientHandler = new ClientHandler(clientSocket);
@@ -51,75 +50,146 @@ public class newChatServer {
             reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             writer = new PrintWriter(clientSocket.getOutputStream(), true);
         }
+//    Original Function, Leave it here just for now
+//    @Override
+//    public void run() {
+//        try {
+//            // Read the user's nickname from the client
+//            String line = reader.readLine();
+//            if (line.startsWith("join ")) {
+//                username = line.substring(5);
+//                System.out.println("[INFO] User " + username + " joined the chat!");
+//                broadcast("\n[INFO] " + username + " has joined the chat!");
+//                broadcast("join " + username);
+//            }
+//
+//            InputStream inputStream = clientSocket.getInputStream();
+//            while (true) {
+//                line = reader.readLine();
+//                if (line == null || line.equals("quit")) {
+//                    broadcast("quit " + username);
+//                    break;
+//                }
+//                System.out.println(username + ": " + line);
+//                broadcast("\n" + username + ": " + line);
+//
+//                // The client has sent a file
+//                if (line.startsWith("file ")) {
+//                    String fileName = line.substring(5);
+//                    byte[] fileContentBytes = readBytesFromStream(inputStream);
+//                    String savePath = "I:/CODE/DATA/" + fileName;
+//                    // Write file to disk
+//                    FileOutputStream fileOutputStream = new FileOutputStream(savePath);
+//                    fileOutputStream.write(fileContentBytes);
+//                    fileOutputStream.close();
+//
+//                    broadcast(username + " just sent a file: " + fileName);
+//                    broadcast("file " + fileName);
+//
+//                    if (getFileExtension(fileName).equalsIgnoreCase("txt")) {
+//                        //TODO: Do something with txt
+//                    }
+//                    // Send file to all clients
+//                    for (ClientHandler handler : clientHandlers) {
+//                        if (handler != this) {
+//                            handler.writer.println("file " + fileName + "\n");
+//                            handler.clientSocket.getOutputStream().write(fileContentBytes);
+//                        }
+//                    }
+//                }
+//            }
+    @Override
+    public void run() {
+        try {
+            DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
 
-        @Override
-        public void run() {
-            try {
+            int fileNameLength = dataInputStream.readInt();
 
-                // Read the user's nickname from the client
-                String line = reader.readLine();
-                if (line.startsWith("join ")) {
-                    username = line.substring(5);
-                    System.out.println("[INFO] User " + username + "joined the chat!");
-                    broadcast("[INFO] " + username + " has joined the chat!");
-                    broadcast("join " + username);
-                }
+            if (fileNameLength > 0) {
+                byte[] fileNameBytes = new byte[fileNameLength];
+                dataInputStream.readFully(fileNameBytes, 0, fileNameBytes.length);
+                String fileName = new String(fileNameBytes);
 
-                // Read messages from the client and broadcast them to all other clients
-                while (true) {
+                int fileContentLength = dataInputStream.readInt();
 
-                    line = reader.readLine();
-                    if (line == null || line.equals("quit")) {
-                        broadcast("quit " + username);
-                        break;
-                    }
-                    System.out.println(username + ": " + line);
-                    broadcast("\n" + username + ": " + line);
+                if (fileContentLength > 0) {
+                    byte[] fileContentBytes = new byte[fileContentLength];
+                    dataInputStream.readFully(fileContentBytes, 0, fileContentLength);
 
+                    String savePath = "I:/CODE/DATA/" + fileName;
 
-                }
-
-                DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
-
-                int fileNameLength = dataInputStream.readInt();
-
-                if (fileNameLength > 0) {
-                    byte[] fileNameBytes = new byte[fileNameLength];
-                    dataInputStream.readFully(fileNameBytes, 0, fileNameBytes.length);
-                    String fileName = new String(fileNameBytes);
-
-                    int fileContentLength = dataInputStream.readInt();
-
-                    if (fileContentLength > 0) {
-                        byte[] fileContentBytes = new byte[fileContentLength];
-                        dataInputStream.readFully(fileContentBytes, 0, fileContentLength);
-
+                    if (getFileExtension(fileName).equalsIgnoreCase("txt")) {
+                        String txtContent = new String(fileContentBytes);
+                        // Write file to disk
+                        FileOutputStream fileOutputStream = new FileOutputStream(savePath);
+                        fileOutputStream.write(fileContentBytes);
+                        fileOutputStream.close();
                         broadcast(username + " just send a file: " +fileName);
-
-                        if (getFileExtension(fileName).equalsIgnoreCase("txt")) {
-                            //TODO: Do something shit with txt
-
-                        }
-
-                        // send file to all clients
-                        for (ClientHandler handler : clientHandlers) {
-                            if (handler != this) {
-                                handler.writer.println("file " + fileName);
-                                handler.clientSocket.getOutputStream().write(fileContentBytes); //TODO: I can't just return this damn metadata, do something with it!
-                            }
-                        }
+                        broadcast(txtContent);
+                        System.out.println("A new file \"" + fileName + "\" has been saved to " + savePath + " !");
+                        //TODO: Do something shit with txt
+                    } else if (getFileExtension(fileName).equalsIgnoreCase("png")) {
+                        // Write file to disk
+                        FileOutputStream fileOutputStream = new FileOutputStream(savePath);
+                        fileOutputStream.write(fileContentBytes);
+                        fileOutputStream.close();
+                        broadcast(username + " just send a file: " +fileName);
+                        System.out.println("A new file \"" + fileName + "\" has been saved to " + savePath + " !");
                     }
+
+    //                // send file to all clients
+    //                for (ClientHandler handler : clientHandlers) {
+    //                    if (handler != this) {
+    //                        handler.writer.println("file " + fileName);
+    //                        handler.clientSocket.getOutputStream().write(fileContentBytes); //TODO: I can't just return this damn metadata, do something with it!
+    //                    }
+    //                }
                 }
-
-
-                // Handle client disconnection
-                System.out.println("[INFO] User " + username + " left the chat!");
-                broadcast("[INFO] " + username + " has left the chat!");
-                clientHandlers.remove(this);
-                clientSocket.close();
-            } catch (IOException e) {
-                System.out.println("Error handling client connection: " + e.getMessage());
             }
+
+        // Read the user's nickname from the client
+        String line = reader.readLine();
+        if (line.startsWith("join ")) {
+            username = line.substring(5);
+            System.out.println("[INFO] User " + username + "joined the chat!");
+            broadcast("[INFO] " + username + " has joined the chat!");
+            broadcast("join " + username);
+        }
+
+        // Read messages from the client and broadcast them to all other clients
+        while (true) {
+
+            line = reader.readLine();
+            if (line == null || line.equals("quit")) {
+                broadcast("quit " + username);
+                break;
+            }
+            System.out.println(username + ": " + line);
+            broadcast("\n" + username + ": " + line);
+
+
+        }
+
+            // Handle client disconnection
+            System.out.println("[INFO] User " + username + " left the chat!");
+            broadcast("[INFO] " + username + " has left the chat!");
+            clientHandlers.remove(this);
+            clientSocket.close();
+        } catch (IOException e) {
+            System.out.println("Error handling client connection: " + e.getMessage());
+        }
+    }
+
+        // Helper method to read all bytes from an input stream
+        private byte[] readBytesFromStream(InputStream inputStream) throws IOException {
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            byte[] data = new byte[1024];
+            int length;
+            while ((length = inputStream.read(data, 0, data.length)) != -1) {
+                buffer.write(data, 0, length);
+            }
+            buffer.flush();
+            return buffer.toByteArray();
         }
 
         // Broadcast a message to all connected clients
@@ -130,10 +200,7 @@ public class newChatServer {
                 }
             }
         }
-
-
     }
-
 
     public static String getFileExtension(String fileName) {
         //This would not work with .tar.gz
@@ -148,6 +215,7 @@ public class newChatServer {
     public static void main(String[] args) throws IOException {
         newChatServer server = new newChatServer(2333);
         server.start();
+
     }
 
     public void Myfile (int id, String name, byte[] data, String fileExtension) {
